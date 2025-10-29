b940e9a6d92916039d13241fc2876bf11e6a5316
Ahmet ÖZDEMİR <94064510+ahmetozdemirrr@users.noreply.github.com>
2025-10-30 01:23:08 +0300

    Update main.c: adding dummy main function

--- src/main.c
+++ src/main.c
@@ -10,3 +10,19 @@
         * feeding the LLVM backend with IR generated from the AST.
         * entegration of GNU toolchains (assembler, linker etc.)
 */
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    printf("Alanc Compiler is Work...\n");
+    
+    if (argc < 2) 
+	{
+        printf("Usage: ./alanc <file_name>\n");
+        return 1;
+	}  
+    (void)argv;
+	
+    return 0;
+}
