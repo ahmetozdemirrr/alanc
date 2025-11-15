3edb7759b3879270872481efdf6d6740573a09fe
Ahmet ÖZDEMİR <94064510+ahmetozdemirrr@users.noreply.github.com>
2025-11-15 23:12:41 +0300

    Add local includes to util_alanc_types.h
    Added local includes section to header file.

--- src/common/include/util_alanc_types.h
+++ src/common/include/util_alanc_types.h
@@ -3,6 +3,9 @@
 #ifndef UTIL_ALANC_TYPES_H
 #define UTIL_ALANC_TYPES_H
 
+/* local includes */
+#include <alanc_types.h>
+
 const char* str_bool(ALANC_BOOLEAN_TYPE value);
 
 #endif /* UTIL_ALANC_TYPES_H */
